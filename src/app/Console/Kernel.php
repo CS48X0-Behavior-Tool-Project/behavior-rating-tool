68cd55034673ef28b140f8d541d550d7d372f989
Christopher MacDonald <chrmacdonal5@upei.ca>
2021-01-21 16:46:33 -0400

    Removed unused import

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Jobs\GetFood;
 
 class Kernel extends ConsoleKernel
 {

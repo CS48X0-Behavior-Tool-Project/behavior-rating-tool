a5569ebd5b5cc0c23381e3f1890875e920a06868
Christopher MacDonald <chrmacdonal5@upei.ca>
2021-01-21 16:07:51 -0400

    Swapped Breeze with UI Bootstrap
    Was an annoying process - required a full reinstallation of Laravel and Bootstrap is delivered via CDN since the local resources seem to be broken.

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\GetFood;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
         $schedule->command('cache:clear')->hourly();
     }
 

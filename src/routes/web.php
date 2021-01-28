580a332d4d1b8b773df51ae069388721445db64d
zmclure <43078156+zmclure@users.noreply.github.com>
2021-01-27 21:56:55 -0400

    Added some comments on initial routing thoughts

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,23 +11,38 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
 
-//login page is the landing page when we first visit the website
+
+/**
+* Currently operating under the assumption that front end people are making blade templates for
+* the approved designs, so I made these initial routes lead to simple HTML pages with still images
+* of our designs.  These can be replaced later with the actual templates/views.
+*/
+
+/**
+* Login page is the landing page when we first visit the website
+*/
 Route::get('/', function () {
     return view('pages.login');
 });
 
 
-//account creation/confirmation page
+/**
+* Account creation/confirmation page
+*/
 Route::get('/confirmation', function () {
     return view('pages.confirmation');
 });
 
-//add user page
+/**
+* Add user page
+*/
 Route::get('/adduser', function () {
     return view('pages.adduser');
 });
 
-//account management page (first/last names, email, password changes)
+/**
+* Account management page (first/last names, email, password changes)
+*/
 Route::get('/account', function () {
     return view('pages.account');
 });

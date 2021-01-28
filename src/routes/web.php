15854ab80aa42816a7500cb97803e2e85549ea99
zmclure <43078156+zmclure@users.noreply.github.com>
2021-01-27 21:44:05 -0400

    put blade templates into a pages directory

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,21 +13,21 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 //login page is the landing page when we first visit the website
 Route::get('/', function () {
-    return view('login');
+    return view('pages.login');
 });
 
 
 //account creation/confirmation page
 Route::get('/confirmation', function () {
-    return view('confirmation');
+    return view('pages.confirmation');
 });
 
 //add user page
 Route::get('/adduser', function () {
-    return view('adduser');
+    return view('pages.adduser');
 });
 
 //account management page (first/last names, email, password changes)
 Route::get('/account', function () {
-    return view('account');
+    return view('pages.account');
 });

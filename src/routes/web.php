c0a1c81a17d6975745ccf3836a0f513dea5d8046
zmclure <43078156+zmclure@users.noreply.github.com>
2021-01-27 21:05:56 -0400

    starting to set up routing between pages

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,8 +5,29 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
+
+//login page is the landing page when we first visit the website
+Route::get('/', function () {
+    return view('login');
+});
+
+
+//account creation/confirmation page
+Route::get('/confirmation', function () {
+    return view('confirmation');
+});
+
+//add user page
+Route::get('/adduser', function () {
+    return view('adduser');
+});
+
+//account management page (first/last names, email, password changes)
+Route::get('/account', function () {
+    return view('account');
+});

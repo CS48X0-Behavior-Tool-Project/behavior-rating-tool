575447f75437281cd8c325f92ec436ba8deb82f3
Mandy <aisenor@upei.ca>
2021-01-29 16:05:39 -0400

    Removed login navigation from login page

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -51,12 +51,6 @@
                         <!-- TODO -->
                         <!-- Will remove these links once we have working login and registration within -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>

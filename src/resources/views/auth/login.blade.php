34dc86d82ec80cd5d0fce1d55d57c53298bac376
Mandy <aisenor@upei.ca>
2021-01-29 13:21:13 -0400

    Removed styles

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<link href="{{ asset('././css/styles.css') }}" rel="stylesheet">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">

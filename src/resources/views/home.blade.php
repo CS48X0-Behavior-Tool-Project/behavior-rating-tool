7150568e7aa9700546e35acb18948506596fc8d2
Mandy <aisenor@upei.ca>
2021-01-29 13:00:39 -0400

    test change

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are totally logged in!') }}
                 </div>
             </div>
         </div>

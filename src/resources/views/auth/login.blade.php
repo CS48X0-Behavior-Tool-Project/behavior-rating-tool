a57d1b71fbe737dbd45055d85da620b064c7354d
Mandy <aisenor@upei.ca>
2021-01-29 13:08:30 -0400

    Rearrange login

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -57,11 +57,17 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                              @if (Route::has('password.request'))
+                                  <a class="btn btn-link" href="{{ route('password.request') }}">
+                                      {{ __('Forgot Your Password?') }}
+                                  </a>
+                              @endif
                             </div>
                         </div>
                     </form>

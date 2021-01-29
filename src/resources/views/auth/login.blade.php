dd9afa1bb54e2443f0558169937a366c7a6d4e66
Mandy <aisenor@upei.ca>
2021-01-29 14:01:17 -0400

    color

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" color="orange">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 

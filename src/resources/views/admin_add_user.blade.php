2b9ce1effdd5116dca8656ed205df2e442ce3dfd
Mandy <aisenor@upei.ca>
2021-02-02 16:22:44 -0400

    Added last name field

--- src/resources/views/admin_add_user.blade.php
+++ src/resources/views/admin_add_user.blade.php
@@ -12,9 +12,21 @@
                       @csrf
                       <!-- Name -->
                       <div class="form-group row">
-                          <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('New User Name') }}</label>
+                          <label for="fname" class="col-md-4 col-form-label text-md-right">{{ __('New User First Name') }}</label>
                           <div class="col-md-6">
-                              <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="John Smith" required autofocus>
+                              <input id="fname" type="text" class="form-control @error('name') is-invalid @enderror" name="fname" placeholder="John" required autofocus>
+                              @error('name')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+                      <div class="form-group row">
+                          <label for="lname" class="col-md-4 col-form-label text-md-right">{{ __('New User Last Name') }}</label>
+                          <div class="col-md-6">
+                              <input id="lname" type="text" class="form-control @error('name') is-invalid @enderror" name="lname" placeholder="Smith" required autofocus>
                               @error('name')
                                   <span class="invalid-feedback" role="alert">
                                       <strong>{{ $message }}</strong>

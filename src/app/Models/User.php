c58054ec9ba7961b4892cd55cfdeac08a72f3a70
Christopher MacDonald <chrmacdonal5@upei.ca>
2021-02-07 17:04:44 -0400

    Added Bouncer to project

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRolesAndAbilities;
 
     /**
      * The attributes that are mass assignable.

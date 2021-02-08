9f914c3eb4967025632996ece4ed51d665d9da1f
ekmu <erik.moraru@gmail.com>
2021-02-08 03:38:52 -0400

    Merge branch 'main' of github.com:CS48X0-Behavior-Tool-Project/behavior-rating-tool into feature/user-creation-testing

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\QuizController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VideoController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,48 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+|--------------------------------------------------------------------------
+| API Under Development
+|--------------------------------------------------------------------------
+|
+| The API below is simply a rough scaffolding of what our API will look like
+| as it currently is non functional (needs corresponding controllers) and
+| and functions. Take care that these are created before calling them.
+| These functions are currently visible to everyone, and eventually they
+| should have properly implemented authorization using Bouncer. Please
+| organize all associated API under the respective header comment.
+|
+*/
+
+/**
+ * Quiz Related API
+ */
+
+Route::get('quizzes', [QuizController::class, 'getAllQuizzes']);
+Route::get('quizzes/{id}', [QuizController::class, 'getQuiz']);
+Route::get('quizzes/{id}/attempts', [QuizController::class, 'getQuizAttempts']);
+Route::post('quizzes/create', [QuizController::class, 'createQuiz']);
+Route::put('quizzes/{id}', [QuizController::class, 'updateQuiz']);
+Route::delete('quizzes/{id}', [QuizController::class, 'deleteQuiz']);
+
+/**
+ * User Related API
+ */
+
+Route::get('users', [UserController::class, 'getAllUsers']);
+Route::get('users/{id}', [UserController::class, 'getUser']);
+Route::get('users/{id}/attempts', [UserController::class, 'getUserAttempts']);
+Route::post('users/create', [UserController::class, 'createUser']);
+Route::put('users/{id}', [UserController::class, 'updateUser']);
+Route::delete('users/{id}', [UserController::class, 'deleteUser']);
+
+/**
+ * Video Related API
+ */
+
+Route::get('videos', [VideoController::class, 'getAllVideos']);
+Route::get('videos/{id}', [VideoController::class, 'getVideo']);
+Route::post('videos/upload', [VideoController::class, 'uploadVideo']);
+Route::delete('videos/{id}', [VideoController::class, 'deleteVideo']);

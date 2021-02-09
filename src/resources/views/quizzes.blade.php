99632154285b8e9d47ed216b326e7e3a11554226
Mandy <aisenor@upei.ca>
2021-02-09 10:07:42 -0400

    Preliminary search bar

--- src/resources/views/quizzes.blade.php
+++ src/resources/views/quizzes.blade.php
@@ -1,11 +1,124 @@
 @extends('layouts.app')
 
+@if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+@endif
+
 @section('content')
 <div class="container">
+    <div class="row col-12">
+        <div class="card col-4">
+            <div class="card-header">Filter</div>
+            <div class="card-body">
+                <input id="search" class="form-control" type="text" name="search" placeholder="Search Quizzes..." onkeyup"">
+
+                <!-- Number of attempts filter -->
+                <div class="form-group row justify-content-center">
+                    <br><p style="text-align:center;">Filter quizzes by your number of attempts</p>
+                    <div>
+                        <span>
+                            <input type="checkbox" id="one" name="one" value="one">
+                            <label for="one"> One</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="two" name="two" value="two">
+                            <label for="two"> Two</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="three" name="three" value="three">
+                            <label for="three"> Three</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="four" name="four" value="four">
+                            <label for="four"> Four</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="five_plus" name="five_plus" value="five_plus">
+                            <label for="five_plus"> Five +</label><br>
+                        </span>
+                    </div>
+                </div>
+                <!-- Animal filter -->
+                <div class="form-group row justify-content-center">
+                    <br><p style="text-align:center;">Filter quizzes by animal</p>
+                    <div>
+                        <span>
+                            <input type="checkbox" id="1" name="1" value="1">
+                            <label for="1"> This will need</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="2" name="2" value="2">
+                            <label for="2"> to be auto-populated</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="3" name="3" value="3">
+                            <label for="3"> from the database</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="4" name="4" value="4">
+                            <label for="4"> based on what animals</label><br>
+                        </span>
+                        <span>
+                            <input type="checkbox" id="5" name="5" value="5">
+                            <label for="5"> are available</label><br>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card col-8">
+            <div class="card-header">Selection</div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="column">
+                <div class="card">
+                    <div class="card-header">{{ __('Create Quiz') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('Create quizzes if you are an admin!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Create Quiz') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('Create quizzes if you are an admin!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Quizzes') }}</div>
+                <div class="card-header">{{ __('Create Quiz') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +127,7 @@
                         </div>
                     @endif
 
-                    {{ __('View all possible quizzes and select the one you would like to attempt!') }}
+                    {{ __('Create quizzes if you are an admin!') }}
                 </div>
             </div>
         </div>

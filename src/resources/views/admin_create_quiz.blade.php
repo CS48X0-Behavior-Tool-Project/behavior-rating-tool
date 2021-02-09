3d28cdbd042d26b565dd6616cf493ea7dc55422d
Mandy <aisenor@upei.ca>
2021-02-09 10:48:43 -0400

    Video upload fields

--- src/resources/views/admin_create_quiz.blade.php
+++ src/resources/views/admin_create_quiz.blade.php
@@ -9,11 +9,50 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-10">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Create Quiz') }}</div>
-
+                <div class="card-header">{{ __('Create A New Quiz') }}</div>
                 <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <iframe src="https://www.youtube.com/embed/ofGy1zuj2rM" width="col-md-4" height="200"></iframe>
+                            <br>
+                            <br>
+                            <div class="form-group row">
+                                <label for="video-id" class="col-md-4 col-form-label text-md-right">ID</label>
+                                <div class="col-md-8">
+                                    <input id="video-id" type="text" class="form-control" name="video-id" placeholder="ID ..." required>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="video-link" class="col-md-4 col-form-label text-md-right">Link</label>
+                                <div class="col-md-8">
+                                    <input id="video-link" type="text" class="form-control" name="video-link" placeholder="YouTube Link ..." required>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="video-name" class="col-md-4 col-form-label text-md-right">Name</label>
+                                <div class="col-md-8">
+                                    <input id="video-name" type="text" class="form-control" name="video-name" placeholder="If different from ID ..." required>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="col">
+                            that
+                        </div>
+                        <div class="col">
+                            this
+                        </div>
+                    </div>
+                    <form class="" action="index.html" method="post">
+                        <div class="form-group row">
+
+                        </div>
+                    </form>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}

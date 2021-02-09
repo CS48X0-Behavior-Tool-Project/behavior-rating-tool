06cf45d937c1fdb223cd6ea532f9dd9915cda242
Mandy <aisenor@upei.ca>
2021-02-09 11:41:03 -0400

    Full template complete Need to implement adding checkboxes from the input fields

--- src/resources/views/admin_create_quiz.blade.php
+++ src/resources/views/admin_create_quiz.blade.php
@@ -13,11 +13,12 @@
             <div class="card">
                 <div class="card-header">{{ __('Create A New Quiz') }}</div>
                 <div class="card-body">
+                    <form>
                     <div class="row">
                         <div class="col">
                             <iframe src="https://www.youtube.com/embed/ofGy1zuj2rM" width="col-md-4" height="200"></iframe>
                             <br>
-                            <p style="text-align:center;">Video information</p>
+                            <p style="text-align:center; color: black; background-color: #f7f7f7;border-radius: 5px;border: 1px solid #dfdfdf;">Video information</p>
 
                             <div class="form-group row">
                                 <label for="video-link" class="col-md-4 col-form-label text-md-right">Link</label>
@@ -43,8 +44,8 @@
                         </div>
 
                         <div class="col">
-                            <h5 style="text-align:center; color: white; background-color: '#';border-radius: 5px;">Behaviours</h5>
-                            <br>
+                            <p style="text-align:center; color: black; background-color: #f7f7f7;border-radius: 5px;border: 1px solid #dfdfdf;">Behaviours</p>
+
                             <h6 style="text-align:center;">Enter all that you wish to have included in the quiz</h6>
                             <input id="behaviours" type="text" class="form-control" name="behaviours" placeholder="Behaviours ..." required>
                             <br>
@@ -76,14 +77,44 @@
 
                         </div>
                         <div class="col">
-                            this
+                            <p style="text-align:center; color: black; background-color: #f7f7f7;border-radius: 5px;border: 1px solid #dfdfdf;">Interpretation</p>
+                            <h6 style="text-align:center;">Enter all that you wish to have included in the quiz</h6>
+                            <input id="interpretation" type="text" class="form-control" name="interpretation" placeholder="Interpretation ..." required>
+                            <br>
+                            <h6 style="text-align:center;">Select the correct answers</h6>
+                            <div class="form-group row justify-content-center">
+                                <div>
+                                    <span>
+                                        <input type="checkbox" id="one" name="one" value="one">
+                                        <label for="one"> These should</label><br>
+                                    </span>
+                                    <span>
+                                        <input type="checkbox" id="two" name="two" value="two">
+                                        <label for="two"> Autopopulate</label><br>
+                                    </span>
+                                    <span>
+                                        <input type="checkbox" id="three" name="three" value="three">
+                                        <label for="three"> Based on what</label><br>
+                                    </span>
+                                    <span>
+                                        <input type="checkbox" id="four" name="four" value="four">
+                                        <label for="four"> Is entered</label><br>
+                                    </span>
+                                    <span>
+                                        <input type="checkbox" id="five_plus" name="five_plus" value="five_plus">
+                                        <label for="five_plus"> Above</label><br>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <form class="" action="index.html" method="post">
-                        <div class="form-group row">
+                    <div class="row justify-content-center">
+                        <button type="submit" class="btn btn-primary">
+                            Create Video
+                        </button>
+                    </div>
 
-                        </div>
                     </form>
 
 

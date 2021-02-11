1bd721deb93116e092b52412e63b02632dad7cec
zmclure <43078156+zmclure@users.noreply.github.com>
2021-02-11 19:39:50 -0400

    Update NewAccountController.php

--- src/app/Http/Controllers/NewAccountController.php
+++ src/app/Http/Controllers/NewAccountController.php
@@ -14,16 +14,19 @@ class NewAccountController extends Controller
   * Called when /confirmation gets a post request from the page.
   */
   public function createAccount() {
-    $surveyResponse = request()->input('experience');
 
+    //set this user's password to their choice
     auth()->user()->fill([
               'password' => Hash::make(request()->input('password'))
           ])->save();
 
+    $surveyResponse = request()->input('experience');
+
+
     // TODO: make sure we are currently logged in to the proper user (done via email link?)
-    // TODO: change the current default password to the user's new choice
     // TODO: store survey choice in json object in users tables
-    // TODO: redirect to home page when finished  (find a way to block future access?)
+    // TODO: find a way to block future access to this page, they only need to do the survey once, and
+    //changing password needs to be done on account management page
 
     return redirect()->route('home_route');
   }

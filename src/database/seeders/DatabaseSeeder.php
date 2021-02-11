45b3c1cf89896ba3337e562f29920468fedb74ae
Christopher MacDonald <chrmacdonal5@upei.ca>
2021-02-11 18:51:09 -0400

    Removed redundant code

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,10 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class
-        ]);
-
-        $this->call([
+            UserSeeder::class,
             SqlFileSeeder::class
         ]);
     }

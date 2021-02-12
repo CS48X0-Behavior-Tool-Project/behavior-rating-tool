33a2944373de54eedc01e00f235c513f272f95d6
Christopher MacDonald <chrmacdonal5@upei.ca>
2021-02-12 12:58:03 -0400

    Added descriptive comments

--- src/resources/views/quiz_attempt.blade.php
+++ src/resources/views/quiz_attempt.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
+{{-- Anytime you're using Video.js include this script for IE8 compatibility --}}
 @section('view_specific_scripts')
     <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 @endsection
 
+{{-- Anytime you're using Video.js these stylesheets --}}
 @section('view_specific_styles')
     <link href="https://vjs.zencdn.net/7.10.2/video-js.css" rel="stylesheet" />
     <link href="{{ asset('/css/styles.css') }}" rel="stylesheet">
@@ -14,24 +16,11 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Attempting Quiz') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('Attempt a quiz!') }}
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-8">
-                <div class="card">
                     <div class="card-header">{{ __('Quiz Video') }}</div>
-
                     <div class="card-body">
+
+                        {{-- Be sure to include the video-wrapper div, as it's CSS components
+                        will ensure that the video.js player is contained within the parent div --}}
                         <div class="video-wrapper">
                             <video id="quiz-video" class="video-js" controls="true" preload="auto" width="auto"
                                 height="auto" data-setup="{'responsive': true}">
@@ -44,6 +33,7 @@
                                 </p>
                             </video>
                         </div>
+                        {{-- Depending on how quiz execution is handled, this video.js should be created via Javascript --}}
                     </div>
                 </div>
             </div>
@@ -51,6 +41,7 @@
     </div>
 @endsection
 
+{{-- Anytime you're using Video.js, use this CDN --}}
 @section('body_end_scripts')
     <script src="https://vjs.zencdn.net/7.10.2/video.min.js"></script>
 @endsection

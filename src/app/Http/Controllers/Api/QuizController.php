c21a0c32412b6c77977fe580ac88e5b32f90d2c8
Tami Liu <tami.teng.liu@gmail.com>
2021-02-17 03:07:31 -0400

    added quiz table, get quizzes api, get quiz/id api

--- src/app/Http/Controllers/Api/QuizController.php
+++ src/app/Http/Controllers/Api/QuizController.php
@@ -4,22 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Quiz;
 
 class QuizController extends Controller
 {
     public function getAllQuizzes()
     {
         // Implement logic to fetch all quizzes
+        $quizzes = Quiz::all();    // TODO: need to add the quiz answer option list
+        return $quizzes;
     }
 
     public function getQuiz($id)
     {
         // Implement logic to fetch quiz
+        return Quiz::find($id);
     }
 
     public function getQuizAttempts($id)
     {
         // Implement logic to fetch quiz attempts
+        
     }
 
     public function createQuiz(Request $request)
@@ -35,5 +40,6 @@ class QuizController extends Controller
     public function deleteQuiz($id)
     {
         // Implement logic to delete quiz
+        Quiz::find($id)->delete();
     }
 }
---
+++ src/database/migrations/2021_02_17_054326_create_quiz_questions.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // quiz_question
+        // id | code | video | question | options
+        // 1  | house-01 | http://google.. | choose behavior and its corresponding interpretations | 
+        // 2  | cat-01 | http://google.. | choose behavior and its corresponding interpretations | 
+       
+        // INSERT INTO quiz_questions(code, video, question)
+        // VALUES ('horse001', 'https://www.youtube.com/watch?v=8bgnG4Ni9Jo', 'Please indicate behaviors and interpretations')
+
+        Schema::create('quiz_questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('video')->nullable();
+            $table->string('question')->nullable();
+            $table->timestamps();
+            $table->jsonb('options')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('quiz_questions');
+        Schema::enableForeignKeyConstraints();
+    }
+}

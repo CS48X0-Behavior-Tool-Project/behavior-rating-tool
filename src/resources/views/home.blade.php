93383ac22ab4517cd35afb191b47f6a42ae7a631
Mandy <aisenor@upei.ca>
2021-02-18 11:11:26 -0400

    Updated home page

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -7,27 +7,23 @@
             <div class="card">
                 <div class="card-header">{{ __('Home') }}</div>
 
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Welcome!') }}
+                    Hi {{ Auth::user()->first_name }} {{ Auth::user()->last_name }},
+                    <br>You are currently logged in as a {{ Auth::user()->roles[0]->name }}
                 </div>
-
-                <form action="{{route('users.destroy', ['user' => Auth::user()->id])}}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">delete</button>
-                </form>
 
                 @if($message = Session::get('success'))
                     <div class="alert alert-success">
                        <strong>{{ $message }}</strong>
                     </div>
                 @endif
+
             </div>
         </div>
     </div>

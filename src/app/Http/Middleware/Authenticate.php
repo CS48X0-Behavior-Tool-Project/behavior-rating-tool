16b49fba427e631423ea9c7597103a3a418b739f
Tami Liu <tami.teng.liu@gmail.com>
2021-02-19 09:54:12 -0400

    removed sql files and unnecessary files i created for user db model

--- src/app/Http/Middleware/Authenticate.php
+++ src/app/Http/Middleware/Authenticate.php
@@ -17,12 +17,5 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
-
-        // tami added for testing
-        $user = $request->user();
-        dd($user->hasRole('developer')); //will return true, if user has role
-        dd($user->givePermissionsTo('create-tasks'));// will return permission, if not null
-        dd($user->can('create-tasks')); // will return true, if user has permission
-
     }
 }
--- src/config/app.php
+++ src/config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\PermissionsServiceProvider::class,
         Silber\Bouncer\BouncerServiceProvider::class
     ],
 

40a93ee24a0687f5c58fcfc7c87d2bdffa83731a
Mandy <aisenor@upei.ca>
2021-02-20 13:29:22 -0400

    Edit home page view to allow for no role

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -14,8 +14,8 @@
                 @endif
 
                 <div class="card-body">
-                    Hi {{ Auth::user()->first_name }} {{ Auth::user()->last_name }},
-                    <br>You are currently logged in as a {{ Auth::user()->roles->pluck('name')}}
+                    Hi {{ Auth::user()->first_name ?? 'John'}} {{ Auth::user()->last_name ?? 'Smith'}},
+                    <br>You are currently logged in as a {{ Auth::user()->roles[0]->name ?? 'Student'}}
                 </div>
 
                 @if($message = Session::get('success'))

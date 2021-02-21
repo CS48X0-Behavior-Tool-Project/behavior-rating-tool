e976c76158e740f1c8e9e2564e44b015a56eb16e
Christopher MacDonald <chrmacdonal5@upei.ca>
2021-02-20 22:17:28 -0400

    Added admin view function
    Required for admin only specific pages

--- src/app/Http/Controllers/PagesController.php
+++ src/app/Http/Controllers/PagesController.php
@@ -2,39 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use Bouncer;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     /**
-    * These controller methods simply load up the appropriate views from the pages folder.
-    */
+     * These controller methods simply load up the appropriate views from the pages folder.
+     */
 
-    public function getLoginPage() {
-      return view('auth.login');
+    public function getLoginPage()
+    {
+        return view('auth.login');
     }
 
-    public function getHomePage() {
-      return view('home');
+    public function getHomePage()
+    {
+        return view('home');
     }
 
-    public function getConfirmationPage() {
-      return view('account_creation');
+    public function getConfirmationPage()
+    {
+        return view('account_creation');
     }
 
-    public function getQuizList() {
-      return view('quizzes');
+    public function getQuizList()
+    {
+        return view('quizzes');
     }
 
-    public function getCreateQuiz() {
-      return view('admin_create_quiz');
+    public function getCreateQuiz()
+    {
+        return $this->adminView(request(), 'admin_create_quiz');
     }
 
-    public function getAddUser() {
-      return view('admin_add_user');
+    public function getAddUser()
+    {
+        return $this->adminView(request(), 'admin_add_user');
     }
 
-    public function getAccountManagement() {
-      return view('account');
+    public function getAccountManagement()
+    {
+        return view('account');
+    }
+
+    private function adminView(Request $request, $path)
+    {
+        if ($request->user()->isAn('admin')) {
+            return view($path);
+        } else {
+            return abort(404);
+        }
     }
 }

5997cb6b733ad83bd43b42be2c66e10295cc2681
zmclure <43078156+zmclure@users.noreply.github.com>
2021-03-13 01:45:09 -0400

    Connect each button to their functions

--- src/app/Http/Controllers/SingleUserController.php
+++ src/app/Http/Controllers/SingleUserController.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 class SingleUserController extends Controller
 {
     public function action($id) {
-      echo 'User id:  ' . $id;
+      if (isset($_POST['reset-password'])) {
+        $this->resetPassword($id);
+      }
+      else if (isset($_POST['delete-user'])) {
+        $this->deleteUser($id);
+      }
+      else if (isset($_POST['view-quizzes'])) {
+        $this->viewUserQuizzes($id);
+      }
+    }
+
+    private function resetPassword($id) {
+      echo 'reset password';
+    }
+
+    private function deleteUser($id) {
+      echo 'delete user';
+    }
+
+    private function viewUserQuizzes($id) {
+      echo 'view user quizzes';
     }
 }

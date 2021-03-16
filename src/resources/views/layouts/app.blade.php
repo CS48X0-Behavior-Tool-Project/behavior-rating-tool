0bea32e4cc9b0d1bc70ff7a60c6b8f61a54a0c6e
Mandy <aisenor@upei.ca>
2021-03-16 18:30:49 -0300

    Reworked nav bar

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -19,14 +19,9 @@
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- <link href="{{ asset('/css/styles.css') }}" rel="stylesheet"> -->
 
     <!-- Page Specific Scripts -->
     @yield('view_specific_scripts')
@@ -35,76 +30,62 @@
     @yield('view_specific_styles')
 </head>
 
+<style media="screen">
+  .nav-link {
+  color: white;
+  }
+</style>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand navbar-dark shadow-sm" style="background-color: #fc8403">
+        <nav class="navbar navbar-expand navbar-dark shadow-sm" style="background-color: #fc8403;">
             <div class="container">
-                @guest
-                    <a class="navbar-brand" href="{{ route('login') }}">
-                        {{ config('app.name') }}
-                    </a>
-                @else
-                    <a class="navbar-brand" href="{{ url('/home') }}">
-                        {{ config('app.name') }}
-                    </a>
-                @endguest
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="nav navbar-dark">
+                      @guest
+                          <a class="navbar-brand" href="{{ route('login') }}">
+                              {{ config('app.name') }}
+                          </a>
+                      @else
+                          <a class="navbar-brand" href="{{ url('/home') }}">
+                              {{ config('app.name') }}
+                          </a>
+                      @endguest
                     </ul>
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
                         @guest
                             <!-- Guests see nothing here -->
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <!-- Admin and TA -->
-                                    @if (Bouncer::is(Auth::user())->an("admin", "ta"))
-                                        <a class="dropdown-item" href="{{ url('add_user') }}">
-                                            {{ __('Add Users') }}
-                                        </a>
-                                        <a class="dropdown-item" href="{{ url('users') }}">
-                                            {{ __('View All Users') }}
-                                        </a>
-                                        <a class="dropdown-item" href="{{ url('create_quiz') }}">
-                                            {{ __('Create New Quiz') }}
-                                        </a>
-                                    @endif
-                                    <!-- Admin, TA, and Experts -->
-                                    @if (Bouncer::is(Auth::user())->an("admin", "ta", "expert"))
-                                        <a class="dropdown-item" href="{{ url('edit_quiz') }}">
-                                            {{ __('Edit Quiz') }}
-                                        </a>
-                                    @endif
-                                    <!-- All users  -->
-                                    <a class="dropdown-item" href="{{ url('quizzes') }}">
-                                        {{ __('Quizzes') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ url('account') }}">
-                                        {{ __('Account') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            <ul class="nav nav-tabs">
+                              <li class="nav-item">
+                                  <!-- Admin and TA -->
+                                  @if (Bouncer::is(Auth::user())->an("admin", "ta"))
+                                      <a class="nav-link active" aria-current="page" style="background-color: #fc8403; color: white;" href="{{ url('users') }}">
+                                          {{ __('Users') }}
+                                      </a>
+                                  @endif
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="{{ url('quizzes') }}">
+                                      {{ __('Quizzes') }}
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="#">Review</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="{{ url('account') }}">
+                                      {{ __('Account') }}
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                  </a>
+                              </li>
+                            </ul>
                         @endguest
-                    </ul>
+
                 </div>
             </div>
         </nav>

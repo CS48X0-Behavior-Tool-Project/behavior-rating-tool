9db77060adfc97fe6f43640f4b2df80560e54eb2
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2021-03-16 20:07:48 -0300

    task: added edit quiz tests

--- src/tests/Feature/QuizTest.php
+++ src/tests/Feature/QuizTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Video;
 use App\Models\Quiz;
 use App\Models\User;
+use Database\Seeders\QuizSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\App;
@@ -20,6 +21,8 @@ class QuizTest extends TestCase
         parent::setUp();
         Bouncer::allow('admin')->to(['delete', 'edit', 'create', 'view'], [User::class, Video::class, Quiz::class]);
         Bouncer::allow('admin')->to('export-users');
+
+        $this->seed(QuizSeeder::class);
 
         // Create a fake video for testing
         Video::create([
@@ -333,5 +336,268 @@ class QuizTest extends TestCase
 
         $response->assertRedirect('/create_quiz');
         $response->assertSessionHas('int-status', 'Selected Field Must Be Filled In');
+    }
+
+    public function test_admin_can_edit_quiz()
+    {
+        $user = $this->createAdmin();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => ['New'],
+                'a-new' => 'Pony',
+                'behaviour-check' => ['1'],
+                'box-one' => 'Kicking',
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => '1',
+                'inter-one' => 'Angry',
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertRedirect('/edit_quiz');
+        $response->assertSessionHas('edit-status', 'Successfully Edited Quiz Horse001');
+    }
+
+    public function test_non_authenticated_user_can_not_edit_quiz()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => ['New'],
+                'a-new' => 'Pony',
+                'behaviour-check' => ['1'],
+                'box-one' => 'Kicking',
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => '1',
+                'inter-one' => 'Angry',
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertStatus(403);
+    }
+
+    public function test_admin_can_not_edit_quiz_with_no_animal_selected()
+    {
+        $user = $this->createAdmin();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => null,
+                'a-new' => 'Pony',
+                'behaviour-check' => ['1'],
+                'box-one' => 'Kicking',
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => '1',
+                'inter-one' => 'Angry',
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertRedirect('/edit_quiz');
+        $response->assertSessionHas('animal-status', 'No Animal Selected');
+    }
+
+    public function test_admin_can_not_edit_quiz_with_no_animal_input()
+    {
+        $user = $this->createAdmin();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => ['New'],
+                'a-new' => null,
+                'behaviour-check' => ['1'],
+                'box-one' => 'Kicking',
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => '1',
+                'inter-one' => 'Angry',
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertRedirect('/edit_quiz');
+        $response->assertSessionHas('animal-status', 'Animal Field Empty');
+    }
+
+    public function test_admin_can_not_edit_quiz_with_invalid_behaviours()
+    {
+        $user = $this->createAdmin();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => ['New'],
+                'a-new' => 'Pony',
+                'behaviour-check' => null,
+                'box-one' => 'Kicking',
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => '1',
+                'inter-one' => 'Angry',
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertRedirect('/edit_quiz');
+        $response->assertSessionHas('behaviour-status', 'Behaviours Incomplete');
+    }
+
+    public function test_admin_can_not_edit_quiz_with_missing_behaviours()
+    {
+        $user = $this->createAdmin();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => ['New'],
+                'a-new' => 'Pony',
+                'behaviour-check' => ['1'],
+                'box-one' => null,
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => '1',
+                'inter-one' => 'Angry',
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertRedirect('/edit_quiz');
+        $response->assertSessionHas('behaviour-status', 'Behaviours Incomplete');
+    }
+
+    public function test_admin_can_not_edit_quiz_with_invalid_interpretations()
+    {
+        $user = $this->createAdmin();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => ['New'],
+                'a-new' => 'Pony',
+                'behaviour-check' => ['1'],
+                'box-one' => 'Kicking',
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => null,
+                'inter-one' => 'Angry',
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertRedirect('/edit_quiz');
+        $response->assertSessionHas('int-status', 'Interpretations Incomplete');
+    }
+
+    public function test_admin_can_not_edit_quiz_with_missing_interpretations()
+    {
+        $user = $this->createAdmin();
+
+        $response = $this->actingAs($user)
+            ->post('/edit_quiz/1', [
+                'video-id' => '1',
+                'video-name' => 'Horse001',
+                'animal-radio' => ['New'],
+                'a-new' => 'Pony',
+                'behaviour-check' => ['1'],
+                'box-one' => 'Kicking',
+                'box-two' => 'Dancing',
+                'box-three' => null,
+                'box-four' => null,
+                'box-five' => null,
+                'box-six' => null,
+                'box-seven' => null,
+                'box-eight' => null,
+                'box-nine' => null,
+                'box-ten' => null,
+                'interpretation-radio' => '1',
+                'inter-one' => null,
+                'inter-two' => 'Laughing',
+                'inter-three' => null,
+                'inter-four' => null,
+                'inter-five' => null,
+            ]);
+
+        $response->assertRedirect('/edit_quiz');
+        $response->assertSessionHas('int-status', 'Interpretations Incomplete');
     }
 }

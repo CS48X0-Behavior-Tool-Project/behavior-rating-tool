34507cb6e0c1b10b0bca06c888a9c6880d4f8b31
Mandy <aisenor@upei.ca>
2021-03-16 20:35:35 -0300

    Attempting new nav bar

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -82,6 +82,9 @@
                                   <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                       {{ __('Logout') }}
                                   </a>
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                      @csrf
+                                  </form>
                               </li>
                             </ul>
                         @endguest

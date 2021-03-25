6b13bcba816866bca825efba0d2d015e19ee9a5d
Mandy <aisenor@upei.ca>
2021-03-25 14:03:59 -0300

    Video thumbnail populates and page resizes appropriately

--- src/resources/views/admin_edit_quiz.blade.php
+++ src/resources/views/admin_edit_quiz.blade.php
@@ -22,8 +22,10 @@
                     @endif
                     <div class="card-body">
                         <div class="row justify-content-center">
-                            <div class="col">
-                                <iframe id="thumbnail" src="videos/{{$quiz->video}}" width="col-md-4" height="200"></iframe>
+                            <div class="col-sm-12 col-md-6 col-lg-4">
+                                <video id="thumbnail" class="" width="100%" style="max-width: 100%" controls>
+                                    <source src="{{ route('videos.show', ['video' => $quiz->video]) }}" type="video/mp4"/>
+                                </video>
                                 <br>
                                 <br>
                                 <p class="title" id="import-video">Import Video</p>
@@ -111,7 +113,7 @@
                                 </div>
 
                             </div>
-                            <div class="col">
+                            <div class="col-sm-12 col-md-3 col-lg-4">
                                 <p class="title" id="behaviour-info">Behaviours</p>
                                 <h6 style="text-align:center;">Enter all the behaviours to included in the quiz</h6>
                                 <p style="text-align:center;">Fields left "Edit me..." or blank will not be included in the quiz</p>
@@ -160,7 +162,7 @@
                                     </div>
                                 @endif
                             </div>
-                            <div class="col">
+                            <div class="col-sm-12 col-md-3 col-lg-4">
                                 <p class="title" id="interpretation-info">Interpretation</p>
                                 <h6 style="text-align:center;">Enter all the interpretations to included in the quiz</h6>
                                 <p style="text-align:center;">Fields left "Edit me..." or blank will not be included in the quiz</p>

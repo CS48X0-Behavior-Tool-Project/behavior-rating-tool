df7e93f118b882ffc432a5e0dfc56ea5e2c1db53
Mandy <aisenor@upei.ca>
2021-04-06 19:13:05 -0300

    Restyling review

--- src/resources/views/review_quiz.blade.php
+++ src/resources/views/review_quiz.blade.php
@@ -1,35 +1,67 @@
 @extends('layouts.app')
 
+@if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+@endif
+
+<link rel="stylesheet" href="{{ URL::asset('css/single_quiz.css') }}">
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Quiz Review:  {{$code ?? 'QuizID'}}  {{'Time Spent ' . $time}}</div>
-
+                <div class="card-header">
+                    <h5>Reviewing Quiz:  {{$code ?? 'QuizID'}}  Attempt {{$attempt ?? '#'}}</h5>
+                    <p>Time Taken:  {{$time ?? '0:00'}}</p>
+                </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div style="text-align: right">
+                        Behaviours: {{$behaviour_score ?? '0'}}/{{$max_behaviour_score ?? '0'}}  Interpretation: {{$interpretation_score ?? '0'}}/1
+                    </div>
+                    <br>
 
-                    <div>
+                    <!-- Video -->
+                    <div class="row justify-content-center">
+                        <video id="thumbnail" class="" width="80%" style="max-width: 100%; resize: both;" controls>
+                            <source src="{{ route('videos.show', ['video' => $video]) }}" type="video/mp4"/>
+                        </video>
+                    </div>
+                    <br>
 
-                    <form action="" method="post">
-                      @csrf
-                        <!-- Stopwatch -->
-                        <div style="text-align:right;">
-                            {{'Score: Behavior ' . $score . ', Interpretation ' . $interpretation_guess}}
-                        </div>
+                    <div class="row">
+                        <!-- Behaviours -->
+                        <div class="col">
+                            <p class="title">Behaviours</p>
+                            <h6>Below you can see the behaviours you select. Those that are correct are highlighted</h6>
+                            <div class="row justify-content-center">
+                                @foreach ($options as $opt)
+                                    @if ( $opt->type == "behaviour")
 
-                        <!-- Video -->
-                        <div class="row justify-content-center">
-                            <video id="thumbnail" class="" width="80%" style="max-width: 100%; resize: both;" controls>
-                                <source src="{{ route('videos.show', ['video' => $video]) }}" type="video/mp4"/>
-                            </video>
+                                        @if(in_array($opt->title, $behavior_answers,true))
+                                            @if ( $opt->is_solution == 1)
+                                                <span style="background-color:#86EA89">
+                                            @else
+                                                <span span style="background-color:yellow">
+                                            @endif
+                                            <input type="checkbox" id="behaviour-check-{{$opt->title}}" name="behaviour-check[]" disabled value="{{$opt->title}}" checked></button>
+                                            <label for="behaviour-check-{{$opt->title}}">{{$opt->title}}</label>
+                                        @else
+                                            <input type="checkbox" id="behaviour-check-{{$opt->title}}" name="behaviour-check[]" disabled value="{{$opt->title}}"></button>
+                                            <label for="behaviour-check-{{$opt->title}}">{{$opt->title}}</label>
+                                        @endif
+
+                                        </span>
+                                        <br>
+                                    @endif
+                                @endforeach
+                            </div>
                         </div>
-                        <br>
+                    </div>
+
+
                         <!-- Questions -->
                         <div class="row">
                             <!-- Behaviours -->
@@ -102,9 +134,7 @@
                                 </div>
                             </div>
                         </div>
-                    </form>
 
-                </div>
             </div>
         </div>
     </div>
